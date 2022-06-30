e8ac7e4c30fa6513d2f792becd27b80bddadc096
dalza1601 <diegoaalza@bcp.com.pe>
2022-06-29 22:13:15 -0500

    feature: TASK-4293 created and implemented unitTest

--- src/test/java/com/pe/dalza/Encuesta_Musical/QuizControllerTests.java
+++ src/test/java/com/pe/dalza/Encuesta_Musical/QuizControllerTests.java
@@ -1,13 +1,27 @@
 package com.pe.dalza.Encuesta_Musical;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pe.dalza.Encuesta_Musical.entity.Quiz;
 import com.pe.dalza.Encuesta_Musical.service.QuizService;
+import com.pe.dalza.Encuesta_Musical.util.JsonUtil;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.List;
 
 @WebMvcTest
+@ExtendWith(MockitoExtension.class)
 public class QuizControllerTests {
     @Autowired
     private MockMvc mockMvc;
@@ -18,6 +32,38 @@ public class QuizControllerTests {
     @Autowired
     private ObjectMapper mapper;
 
+    @Test
+    public void createQuiz_whenPostMethod() throws Exception {
 
+        Quiz quiz = new Quiz();
+        quiz.setTypeMusic("ROCK");
+        quiz.setEmail("dalza.16524@gmail.com");
+
+        BDDMockito.given(quizService.saveQuiz(quiz)).willReturn(quiz);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/quiz")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.toJson(quiz)))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    public void listAllQuizes_whenGetMethod() throws Exception {
+
+        Quiz quiz = new Quiz();
+        quiz.setTypeMusic("ROCK");
+        quiz.setEmail("dalza.16524@gmail.com");
+
+        List<Quiz> quizes = Arrays.asList(quiz);
+
+        BDDMockito.given(quizService
+                        .fetchQuizList())
+                .willReturn(quizes);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/quiz")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data", Matchers.hasSize(1)));
+    }
 
 }
--- src/test/java/com/pe/dalza/Encuesta_Musical/QuizServiceImplTests.java
+++ src/test/java/com/pe/dalza/Encuesta_Musical/QuizServiceImplTests.java
@@ -6,11 +6,14 @@ import com.pe.dalza.Encuesta_Musical.service.Impl.QuizServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @ExtendWith(MockitoExtension.class)
 public class QuizServiceImplTests {
@@ -20,6 +23,47 @@ public class QuizServiceImplTests {
     @InjectMocks
     private QuizServiceImpl quizService;
 
+    @Test
+    public void whenSaveQuizThenReturnQuiz() {
+        Quiz quiz = new Quiz();
+        quiz.setTypeMusic("ROCK");
+        quiz.setEmail("dalza@gmail.com");
+
+        Mockito.when(quizRepository.save(ArgumentMatchers.any(Quiz.class))).thenReturn(quiz);
+        Quiz created = quizService.saveQuiz(quiz);
+        Assertions.assertThat(created.getEmail()).isSameAs(quiz.getEmail());
+        Mockito.verify(quizRepository).save(quiz);
+
+    }
+
+    @Test
+    public void shouldReturnAllQuiz() {
+        List<Quiz> quizes = new ArrayList<>();
+        quizes.add(new Quiz());
+
+        BDDMockito.given(quizRepository.findAll()).willReturn(quizes);
+        List<Quiz> expected = quizService.fetchQuizList();
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, quizes);
+        Mockito.verify(quizRepository).findAll();
+    }
+
+    @Test
+    public void whenGivenId_shouldUpdateQuiz_ifFound() {
+        Quiz quiz = new Quiz();
+        quiz.setEmail("dalzafs@gmail.com");
+        quiz.setTypeMusic("ROCK");
+        quiz.setQuizId(1L);
+
+        Quiz newQuiz = new Quiz();
+        quiz.setTypeMusic("JAZZ");
+
+        BDDMockito.given(quizRepository.findById(quiz.getQuizId())).willReturn(Optional.of(quiz));
+        quizService.updateQuiz(newQuiz, quiz.getQuizId());
+
+        Mockito.verify(quizRepository).save(quiz);
+        Mockito.verify(quizRepository).findById(quiz.getQuizId());
+    }
 
 
 }
